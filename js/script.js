c2620dd2d61028bfe8eacc3cb342c11a52980fd4
Arthur Rodrigues Dantas <164256979+Thuuuur@users.noreply.github.com>
2024-03-26 21:25:19 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navbarMenu = document.querySelector('.navbar-menu');
-    const navbarContainer = document.querySelector('.navbar-container');
-
-    mobileMenu.addEventListener('click', () => {
-        navbarContainer.classList.toggle('active');
-    });
-});
